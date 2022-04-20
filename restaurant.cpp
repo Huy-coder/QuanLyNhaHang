c23b8e9eb96d5f29bb6593f90f974ab4594772f0
Huy-coder <64514659+Huy-coder@users.noreply.github.com>
2022-04-20 21:25:07 +0700

    Add files via upload

--- restaurant.cpp
+++ restaurant.cpp
@@ -28,7 +28,7 @@ struct ORDER
   ORDER *next;
 };
 
-struct PhieuThanhToan
+struct phieuThanhToan
 {
   int codeBill;
   int Sum;
@@ -36,7 +36,7 @@ struct PhieuThanhToan
 
 struct BILL
 {
-  PhieuThanhToan PTT;
+  phieuThanhToan PTT;
   BILL *next;
 };
 
@@ -62,25 +62,6 @@ menu themMonAn() //them mon an vao MENU
   return tmp;
 }
 
-order goiMonAn() //goi mon an tu MENU
-{
-  GoiMon GM;
-  cout<<"Nhap ma mon an can goi: \n";
-  cin>>GM.codeName;
-  cout<<"Nhap so phan can goi: \n";
-  cin>>GM.soPhan;
-
-  order tmp = new ORDER();
-  tmp->GM = GM;
-  tmp->next = NULL;
-  return tmp;
-}
-
-bool empty(menu a) //Check rong
-{
-  return a == NULL;
-}
-
 void themMonAnVaoDanhSach(menu &a) //Them mon an vao list
 {
   menu tmp = themMonAn();
@@ -91,24 +72,6 @@ void themMonAnVaoDanhSach(menu &a) //Them mon an vao list
   else
   {
     menu p = a;
-    while(p->next != NULL)
-    {
-      p = p->next;
-    }
-    p->next = tmp;
-  }
-}
-
-void goiMonTrongDanhSach(order &b) //Goi mon trong list cua Menu
-{
-  order tmp = goiMonAn();
-  if(b == NULL)
-    {
-      b = tmp;
-    }
-  else
-  {
-    order p = b;
     while(p->next != NULL)
     {
       p = p->next;
@@ -141,31 +104,6 @@ void xoaMonAn(menu &a, int pos)
   }
 }
 
-void xoaMonAnDaOrder(order &b, int pos)
-{
-  order tmp = b;
-  if(pos < 0)
-  {
-    cout<<"Khong hop le\n";
-    return;
-  }
-  order before = NULL, after = b;
-  for(int i = 1; i<pos; i++)
-  {
-    before = after;
-    after = after->next;                                                                                                           
-  }
-  if(before == NULL)
-  {
-    b = b->next;
-  }
-  else
-  {   
-    before->next = after->next;
-  }
-} 
-
-
 void inThongTinMonAn(ThucDon TD)
 {
   cout<<"-------------------------------\n";
@@ -184,25 +122,6 @@ void inDanhSachMonAn(menu a)
     a = a->next;
   }
   cout<<"-----------ENDLIST--------------------\n";
-}
-
-void inThongTin1MonAnTrongOrder(GoiMon GM)
-{
-  cout<<"---------------------\n";
-  cout<<"| Ma mon an: "<<GM.codeName<<"      |\n";
-  cout<<"| So phan: " <<GM.soPhan<<"        |\n";
-  cout<<"---------------------\n";
-}
-
-void inDanhSachOrder(order b)
-{
-  cout<<"--------------Ordered------------------\n";
-  while(b != NULL)
-  {
-    inThongTin1MonAnTrongOrder(b->GM);
-    b = b->next;
-  }
-  cout<<"--------------ENDLIST------------------\n";
 }
 
 void timKiemThongTin(menu a, int pos)
@@ -270,14 +189,238 @@ void sapXepMonAn(menu &a)
   }
 }
 
-void goiMonAn(menu a, order &b)
+order goiMonAn() //goi mon an tu MENU
 {
+
+  GoiMon GM;
+  cout<<"Nhap ma mon an can goi: \n";
+  cin>>GM.codeName;
+  cout<<"Nhap so phan can goi: \n";
+  cin>>GM.soPhan;
+
+  order tmp = new ORDER();
+  tmp->GM = GM;
+  tmp->next = NULL;
+  return tmp;
 }
 
+void goiMonTrongDanhSach(order &b) //Goi mon trong list cua Menu
+{
+  order tmp = goiMonAn();
+  if(b == NULL)
+    {
+      b = tmp;
+    }
+  else
+  {
+    order p = b;
+    while(p->next != NULL)
+    {
+      p = p->next;
+    }
+    p->next = tmp;
+  }
+}
+
+void xoaMonAnDaOrder(order &b, int pos)
+{
+  order tmp = b;
+  if(pos < 0)
+  {
+    cout<<"Khong hop le\n";
+    return;
+  }
+  order before = NULL, after = b;
+  for(int i = 1; i<pos; i++)
+  {
+    before = after;
+    after = after->next;                                                                                                           
+  }
+  if(before == NULL)
+  {
+    b = b->next;
+  }
+  else
+  {   
+    before->next = after->next;
+  }
+} 
+
+void inThongTin1MonAnTrongOrder(GoiMon GM)
+{
+  cout<<"---------------------\n";
+  cout<<"| Ma mon an: "<<GM.codeName<<"      |\n";
+  cout<<"| So phan: " <<GM.soPhan<<"        |\n";
+  cout<<"---------------------\n";
+}
+
+void inDanhSachOrder(order b)
+{
+  cout<<"--------------Ordered------------------\n";
+  while(b != NULL)
+  {
+    inThongTin1MonAnTrongOrder(b->GM);
+    b = b->next;
+  }
+  cout<<"--------------ENDLIST------------------\n";
+}
+
+void inThanhTien1MonAn(GoiMon GM)
+{
+  cout<<"Mon ma so "<<GM.codeName<<": "<<GM.thanhTien<<endl;
+}
+
+void inThanhTienNhieuMonAn(order b)
+{
+  long long Sum = 0;
+  cout<<"******************************\n";
+  while(b != NULL)
+  {
+    inThanhTien1MonAn(b->GM);
+    b=b->next;
+  }
+  cout<<"******************************\n";
+}
+
+void chinhSuaSoPhan(order &b, int pos)
+{
+  order tmp = b;
+  while(tmp != NULL)
+  {
+    if(tmp->GM.codeName == pos)
+    {
+      cout<<"Xin moi ban chinh sua so phan an: \n";
+      cin>>tmp->GM.soPhan;
+    }
+    tmp=tmp->next;
+  }
+}
+
+void thanhTienOrder(menu a, order &b)
+{
+  order tmp1 = b;
+  while(tmp1 != NULL)
+  {
+    menu tmp = a;
+    while(tmp != NULL)
+    {
+    if(tmp->TD.codeName == tmp1->GM.codeName)
+      tmp1->GM.thanhTien = tmp->TD.Cost * tmp1->GM.soPhan;
+    tmp = tmp->next;
+    }
+    tmp1 = tmp1->next;
+  }
+}
+
+bill themHoaDon()
+{
+  phieuThanhToan PTT;
+  cout<<"Nhap ma phieu can luu: \n";
+  cin>>PTT.codeBill;
+  cout<<"Hoa don hien tai la: \n";
+  cin>>PTT.Sum;
+
+  bill tmp = new BILL();
+  tmp->PTT = PTT;
+  tmp->next = NULL;
+  return tmp;
+}
+
+void taoBill(bill &c)
+{
+  bill tmp = themHoaDon();
+  if(c == NULL)
+    {
+      c = tmp;
+    }
+  else
+  {
+    bill p = c;
+    while(p->next != NULL)
+    {
+      p = p->next;
+    }
+    p->next = tmp;
+  }
+}
+
+void inThongTin1Bill(phieuThanhToan PTT)
+{
+  cout<<"-----------------------------------\n";
+  cout<<"Ma hoa don: "<<PTT.codeBill<<endl;
+  cout<<"Tong tien cua hoa don: "<<PTT.Sum<<endl;
+  cout<<"----------------------------------\n";
+}
+
+void inDanhSachBill(bill c)
+{
+  cout<<"-------------LIST-----------------\n";
+  while(c != NULL)
+  {
+    inThongTin1Bill(c->PTT);
+    c = c->next;
+  }
+  cout<<"-------------ENDLIST-----------------\n";
+}
+
+void thanhTienTrongBill(order b, bill &c)
+{
+  order tmp1 = b;
+  bill tmp = c;
+  while(tmp != NULL)
+  {
+    
+    if(tmp->PTT.Sum == 0)
+    {
+      while(tmp1 != NULL)
+      {
+        tmp->PTT.Sum = tmp->PTT.Sum + tmp1->GM.thanhTien;
+        tmp1 = tmp1->next;
+      }
+    }
+    tmp = tmp->next;
+  }
+  cout<<"\nTinh toan thanh cong!!!\n";
+}
+
+void timKiemPhieuThanhToan(bill c, int pos)
+{
+  bill tmp = c;
+  while(tmp != NULL)
+  {
+    if(tmp->PTT.codeBill == pos)
+      inThongTin1Bill(tmp->PTT);
+    tmp = tmp->next;
+  }
+}
+
+void sapXepBill(bill &c)
+{
+  for(bill p = c; p->next != NULL; p = p->next)
+  {
+    bill min = p;
+    for(bill q = p->next; q != NULL; q = q->next)
+    {
+      if(q->PTT.Sum < min->PTT.Sum)
+      {  
+        min = q;
+    
+      }
+    }
+    int tmp = min->PTT.codeBill;
+    min->PTT.codeBill = p->PTT.codeBill;
+    p->PTT.codeBill = tmp;
+
+    int tmp1 = min->PTT.Sum;
+    min->PTT.Sum = p->PTT.Sum;
+    p->PTT.Sum = tmp1;
+  }
+}
 int main()
 {
   menu Head = NULL;
   order Head1 = NULL;
+  bill Head2 = NULL;
   while(1)
   {
     cout<<"------------------QUAN LY QUAN CAFE-----------------------\n";
@@ -348,24 +491,57 @@ int main()
         cin>>selectOrder;
         if(selectOrder == 1)
         {
+          inDanhSachMonAn(Head);
+          if(Head != NULL)
+          {
           goiMonTrongDanhSach(Head1);
           inDanhSachOrder(Head1);
+          }
+          else
+          {
+            cout<<"Hien tai khong co mon nao trong thuc don"<<endl;
+          }
         }
         if(selectOrder == 2)
         {
-          cout<<"Vi tri mon an ma ban muon xoa: \n";
-          int pos;
-          cin>>pos;
-          xoaMonAnDaOrder(Head1, pos);
-          inDanhSachOrder(Head1);
+          if(Head != NULL && Head1 != NULL)
+          {
+            cout<<"Vi tri mon an ma ban muon xoa: \n";
+            int pos;
+            cin>>pos;
+            xoaMonAnDaOrder(Head1, pos);
+            inDanhSachOrder(Head1);
+          }
+          else
+          {
+            cout<<"Hien tai khong co mon nao trong thuc don va order de xoa!!"<<endl;
+          }
         }
         if(selectOrder == 3)
         {
-          return 0;
+          if(Head != NULL && Head1 != NULL)
+          {
+            inDanhSachOrder(Head1);
+            cout<<"Xin moi ban nhap ma mon can chinh sua: \n";
+            int codeName;
+            cin>>codeName;
+            chinhSuaSoPhan(Head1, codeName);
+            inDanhSachOrder(Head1);
+            cout<<"Chinh sua thanh cong!!!\n";
+          }
+          else
+          {
+            cout<<"Khong co mon nao xuat hien trong order de chinh sua"<<endl;
+          }
         }
         if(selectOrder == 4)
         {
-          return 0;
+          thanhTienOrder(Head, Head1);
+          inThanhTienNhieuMonAn(Head1);
+          taoBill(Head2);
+          cout<<"Xin vui long cho trong giay lat....";
+          thanhTienTrongBill(Head1, Head2);
+          Head1 = NULL;
         }
     }
     else
@@ -380,15 +556,19 @@ int main()
         cin>>selectBill;
         if(selectBill == 1)
         {
-          return 0;
+          inDanhSachBill(Head2);
         }
         if(selectBill == 2)
         {
-          return 0;
+          int codeBill;
+          cout<<"Moi nhap ma phieu thanh toan: \n";
+          cin>>codeBill;
+          timKiemPhieuThanhToan(Head2, codeBill);
         }
         if(selectBill == 3)
         {
-          return 0;
+          sapXepBill(Head2);
+          cout<<"Sap xep thanh cong\n";
         }
     }
   }
